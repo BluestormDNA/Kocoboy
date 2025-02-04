7ac2a0c04eaa7f9cb2a64f2c8f0176633c3db9a9
Blue <bluestorm.android@gmail.com>
2025-02-04 11:12:24 +0100

    Add MBC2 Cartridge type

---
+++ kocoboy-core/src/commonMain/kotlin/io/github/bluestormdna/kocoboy/core/cartridge/MBC2.kt
@@ -0,0 +1,44 @@
+package io.github.bluestormdna.kocoboy.core.cartridge
+
+
+@OptIn(ExperimentalUnsignedTypes::class)
+class MBC2(private val rom: UByteArray) : Cartridge {
+
+    private val eRam = UByteArray(0x200)
+
+    private var eRamEnabled = false
+    private var romBank = 1
+
+    override fun readLoROM(addr: UShort): UByte {
+        return rom[addr.toInt()]
+    }
+
+    override fun readHiROM(addr: UShort): UByte {
+        return rom[(ROM_OFFSET * romBank) + (addr and 0x3FFFu).toInt()]
+    }
+
+    override fun writeROM(addr: UShort, value: UByte) {
+        when (addr) {
+            in 0x0000u..0x1FFFu -> eRamEnabled = value and 0x01u == 0.toUByte()
+            in 0x2000u..0x3FFFu -> romBank = value.toInt() and 0xF
+        }
+    }
+
+    override fun readERAM(addr: UShort): UByte {
+        return if (eRamEnabled) {
+            eRam[(addr and 0x1FFFu).toInt()]
+        } else {
+            0xFFu
+        }
+    }
+
+    override fun writeERAM(addr: UShort, value: UByte) {
+        if (eRamEnabled) {
+            eRam[(addr and 0x1FFFu).toInt()] = value
+        }
+    }
+
+    companion object {
+        private const val ROM_OFFSET = 0x4000
+    }
+}
