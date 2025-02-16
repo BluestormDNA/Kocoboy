a79168038b605ee8c4ba0bcff843fceba002ba92
Blue <bluestorm.android@gmail.com>
2025-02-16 09:30:08 +0100

    UI: Add palette icon for theme selection

--- composeApp/src/commonMain/kotlin/io/github/bluestormdna/kocoboy/ui/main/SideBar.kt
+++ composeApp/src/commonMain/kotlin/io/github/bluestormdna/kocoboy/ui/main/SideBar.kt
@@ -1,10 +1,11 @@
 package io.github.bluestormdna.kocoboy.ui.main
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,20 +13,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import io.github.bluestormdna.kocoboy.core.cartridge.CartridgeHeader
 import io.github.bluestormdna.kocoboy.ui.dmg.ClassicGrayBody
 import io.github.bluestormdna.kocoboy.ui.dmg.ColorTheme
 import io.github.bluestormdna.kocoboy.ui.dmg.GameBoy
+import io.github.bluestormdna.kocoboy.ui.dmg.UnitShape
 import io.github.bluestormdna.kocoboy.ui.dmg.themeList
 import io.github.bluestormdna.kocoboy.ui.icons.FolderOpen
+import io.github.bluestormdna.kocoboy.ui.icons.Palette
 import io.github.bluestormdna.kocoboy.ui.icons.Power
 import io.github.bluestormdna.kocoboy.ui.theme.LocalColorTheme
 
@@ -41,6 +51,7 @@ fun SideBar(
     Column(
         modifier = modifier.fillMaxHeight().widthIn(max = 200.dp),
     ) {
+        var showThemeList by remember { mutableStateOf(false) }
         Text(
             text = "KocoBoy",
             fontSize = 24.sp,
@@ -59,6 +70,9 @@ fun SideBar(
             IconButton(onPowerSwitch) {
                 Icon(Power, contentDescription = "Power")
             }
+            IconButton(onClick = { showThemeList = !showThemeList }) {
+                Icon(Palette, contentDescription = "Theme selector")
+            }
         }
         Column(
             modifier = Modifier.padding(8.dp)
@@ -67,17 +81,28 @@ fun SideBar(
             Text(cartridgeHeader.type)
         }
         LazyColumn(
-            modifier = Modifier.padding(horizontal = 32.dp),
-            contentPadding = PaddingValues(vertical = 16.dp),
+            modifier = Modifier.padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            items(items = themeList, key = { theme -> theme.body.value.toLong() }) { theme ->
+            items(
+                items = if (showThemeList) themeList else emptyList(),
+                key = { theme -> theme.body.value.toLong() }) { theme ->
                 CompositionLocalProvider(LocalColorTheme provides theme) {
-                    GameBoy(
-                        modifier = Modifier.clickable { onThemeChange(theme) }
-                    )
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(10.dp))
+                            .border(1.dp, color = theme.body, RoundedCornerShape(10.dp))
+                            .clickable { onThemeChange(theme) }
+                            .padding(32.dp)
+                            .animateItem()
+                    ) {
+                        GameBoy(
+                            modifier = Modifier.shadow(8.dp, UnitShape)
+                        )
+                    }
                 }
             }
         }
+
     }
 }
