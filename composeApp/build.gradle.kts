46f6546ed7c38c56ec0879dd6b03a4d6dc52183a
Blue <bluestorm.android@gmail.com>
2025-02-16 09:31:03 +0100

    Project: Bump dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -98,10 +98,10 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
-            //proguardFiles(
-            //    getDefaultProguardFile("proguard-android-optimize.txt"),
-            //    "proguard-rules.pro"
-            //)
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
             signingConfig = signingConfigs.getByName("debug")
         }
     }
